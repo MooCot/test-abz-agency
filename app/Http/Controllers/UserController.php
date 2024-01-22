2e08cb8ef2e0e56df2f60ea37393ecb82bfb7956
Slava Mora <gitmooncat@gmail.com>
2024-01-22 23:06:26 +0200

    work at UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,10 +11,44 @@ use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    public function index(Request $request) {
-        $perPage = 5;
-        $users = User::paginate($perPage);
-        return response()->json(['success' => true, 'users' => $users]);
+    public function index(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'count' => 'int',
+                'page' => 'int|min:1',
+            ]);
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+            $count = $request->input('count', 5);
+            $users = User::paginate($count);
+            if ((int)$users->lastPage() < (int)$request->page) {
+                throw new \Exception("Page not found");
+            }
+            return response()->json([
+                'success' => true,
+                'page' => $users->currentPage(),
+                'total_pages' => $users->lastPage(),
+                'total_users' => $users->total(),
+                'count' => $count,
+                "links" => [
+                    "next_url" => $users->nextPageUrl(),
+                    "prev_url" => $users->previousPageUrl()
+                ],
+                "users" => $users->items()
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'errors' => $e->validator->errors(),
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'errors' => $e->getMessage(),
+            ], 404);
+        }
     }
 
     public function create(CreateUserRequest $request)
@@ -35,22 +69,20 @@ class UserController extends Controller
 
     public function show(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required|nameric',
-        ]);
-
         try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required|nameric',
+            ]);
             if ($validator->fails()) {
                 throw new ValidationException($validator);
             }
-
             $user = User::findOrFail($request->id);
             return response()->json(['success' => true, "user" => $user]);
         } catch (ModelNotFoundException $e) {
             return response()->json([
                 "success" => false,
                 "message" => "The user with the requested identifier does not exist",
-                $e->getMessage()]);
+                $e->getMessage()], 404);
         } catch (ValidationException $e) {
             return response()->json([
                 'success' => false,
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Position::factory(10)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
