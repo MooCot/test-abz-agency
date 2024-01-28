08d3e151ca422203c1c1812935be08626679f60c
Slava Mora <gitmooncat@gmail.com>
2024-01-28 16:17:20 +0200

    fix seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Position;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,8 +25,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'phone' => "+380" . $this->faker->unique()->numberBetween($min = 1000, $max = 9999),
-            'photo' => $this->faker->name(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'photo' => Str::random(10),
+            'password' => Hash::make(Str::random(10)),
             'remember_token' => Str::random(10),
             'positions_id' => $this->faker->randomElement($userIds),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('personal_access_tokens')->insert([
+            'name' => Str::random(10),
+            'token' => Hash::make(Str::random(10)),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
         \App\Models\Position::factory(10)->create();
         \App\Models\User::factory(10)->create();
     }
